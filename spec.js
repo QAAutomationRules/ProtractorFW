a43d4c1cdd223f4f6a5bf20473a5231fd92c036d
= <avzrule@gmail.com>
2017-08-29 14:50:56 -0600

    change page name

--- spec.js
+++ spec.js
@@ -4,51 +4,51 @@
   var CalculatorPage = require('../ProtractorFW/CalculatorPage.js');
 
 describe('Calculator Page', function () {
-    var page;
+    var calculatorPage;
 
 
   beforeEach(function() {
-    page = new CalculatorPage();
+    calculatorPage = new CalculatorPage();
   });
 
   it('History should be added to the table as the calculations are run', function() {
     
-    page.add(1, 2);
+    calculatorPage.add(1, 2);
 
-    expect(page.history.count()).toEqual(1);
+    expect(calculatorPage.history.count()).toEqual(1);
 
-    page.add(3, 4);
+    calculatorPage.add(3, 4);
     
-    expect(page.history.count()).toEqual(2);
+    expect(calculatorPage.history.count()).toEqual(2);
 
-    page.add(5, 6);
+    calculatorPage.add(5, 6);
 
-    expect(page.history.count()).toEqual(3); 
+    expect(calculatorPage.history.count()).toEqual(3); 
 
   });
 
   it('The add number result should display correctly.', function() {
-    page.add(1, 2);
+    calculatorPage.add(1, 2);
 
-    expect(page.result.getText()).toEqual('3');
+    expect(calculatorPage.result.getText()).toEqual('3');
   
-    page.result.getText().then(function(text) {
+    calculatorPage.result.getText().then(function(text) {
       console.log(text);
     });
 
-    page.add(3, 4);
+    calculatorPage.add(3, 4);
 
-    expect(page.result.getText()).toEqual('7');
+    expect(calculatorPage.result.getText()).toEqual('7');
 
-    page.result.getText().then(function(text) {
+    calculatorPage.result.getText().then(function(text) {
       console.log(text);
     });
 
-    page.add(5, 6);
+    calculatorPage.add(5, 6);
 
-    expect(page.result.getText()).toEqual('11');
+    expect(calculatorPage.result.getText()).toEqual('11');
 
-    page.result.getText().then(function(text) {
+    calculatorPage.result.getText().then(function(text) {
       console.log(text);
     });
 
